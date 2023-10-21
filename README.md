fba2e6180af8285847839dbf1a3db25d942ab6d2
Craig Johnson <craigdev937@gmail.com>
2023-10-21 16:18:04 -0400

    first commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Films and Shows
+
+An app created with TypeScript, Webpack, React Router, TMDB API, React, and Redux Toolkit.
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>Films</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ut, voluptatibus provident ipsum mollitia dolor perspiciatis labore fugiat consequuntur ratione? Tempora repellat ex hic, accusamus ipsum in laboriosam ab adipisci?</p>
+        </React.Fragment>
+    );
+};
+
+
